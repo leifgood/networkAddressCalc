cbfbb1f9f7e4e66d350dad2c2a143aff2e837542
IT4-gutole <IT4-gutole@R215-14.g18.de>
2016-06-10 10:23:31 +0200

    Networkpresenter vollständig

--- networkAddressCalc/src/Presenter/NetworkPresenter.java
+++ networkAddressCalc/src/Presenter/NetworkPresenter.java
@@ -2,34 +2,50 @@ package Presenter;
 
 import javax.swing.JOptionPane;
 
+import GUI.AddNetworkDialog;
 import GUI.NetworkFrame;
+import GUI.SubnetDialog;
 import Model.Data;
 import Model.IPv4Address;
+import Model.Network;
 
 public class NetworkPresenter {
 	
 	private NetworkFrame frame;
 	private Data data;
 	
+	public NetworkPresenter(Data data) {
+		this.data = data;
+	}
+	
 	public void verifySave() {
-		
+		try {
+			data.save();
+		} catch (Exception e) {
+			JOptionPane.showConfirmDialog(null, "Error: " + e.getMessage(), "Error", JOptionPane.OK_OPTION);
+		}
 	}
 	
 	public void verifyLoad() {
-		
+		try {
+			Data data = Data.load();
+			this.data = data;
+		} catch (Exception e) {
+			JOptionPane.showConfirmDialog(null, "Error: " + e.getMessage(), "Error", JOptionPane.OK_OPTION);
+		}
 	}
 	
 	public void verifyAdd() {
-//		AddNetworkPresenter addNetworkPresenter = new AddNetworkPresenter(data);
-//		AddNetworkDialog addNetworkDialog = new AddNetworkDialog(addNetworkPresenter);
-//		addNetworkPresenter.setDialog(addNetworkDialog);
-//		addNetworkDialog.setVisible(true);
+		AddNetworkPresenter addNetworkPresenter = new AddNetworkPresenter(data);
+		AddNetworkDialog addNetworkDialog = new AddNetworkDialog(addNetworkPresenter);
+		addNetworkPresenter.setDialog(addNetworkDialog);
+		addNetworkDialog.setVisible(true);
 	}
 	
 	public void verifyDelete() {
 		String selectedItem = frame.getSelectedItem();
 		if (selectedItem != null) {
-			int dialogResult = JOptionPane.showConfirmDialog(null, "Are you sure?", "delete", JOptionPane.YES_NO_OPTION);
+			int dialogResult = JOptionPane.showConfirmDialog(null, "Are you sure?", "Delete", JOptionPane.YES_NO_OPTION);
 			if (dialogResult == JOptionPane.YES_OPTION) {
 				IPv4Address ipv4 = IPv4Address.generateFromString(selectedItem);
 				data.deleteNetwork(data.getNetworkByIPv4(ipv4));
@@ -38,11 +54,14 @@ public class NetworkPresenter {
 	}
 	
 	public void verifySubnets() {
-//		Network network = new Network();
-//		SubnetPresenter subnetPresenter = new SubnetPresenter(network);
-//		SubnetDialog subnetDialog = new SubnetDialog(subnetPresenter);
-//		subnetPresenter.setDialog(subnetDialog);
-//		subnetDialog.setVisible(true);
+		Network network = new Network();
+		SubnetPresenter subnetPresenter = new SubnetPresenter(network);
+		SubnetDialog subnetDialog = new SubnetDialog(subnetPresenter);
+		subnetPresenter.setDialog(subnetDialog);
+		subnetDialog.setVisible(true);
 	}
 
+	public void setFrame(NetworkFrame frame) {
+		this.frame = frame;
+	}
 }
